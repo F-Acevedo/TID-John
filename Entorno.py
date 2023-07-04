f32f0d654522b3fd6693cf0f7adc3cd71c93ea4a
mverschuerenp <martin.verschueren@gmail.com>
2023-07-03 21:17:56 -0400

    Update Entorno.py

--- Entorno.py
+++ Entorno.py
@@ -8,8 +8,8 @@ import pinecone
 import glob
 
 def peticion_gpt(word_1,word_2):
-
-    ## Extraer papers 
+    '''   # Cargar nuevos datos a Pinecone # 
+    ### Extraer papers 
     file_list = glob.glob('papers2/*.txt')
     documentos = []
     for file_path in file_list:
@@ -35,34 +35,24 @@ def peticion_gpt(word_1,word_2):
         environment = 'asia-southeast1-gcp-free')
     ## Subir datos
     docsearch = Pinecone.from_texts([t.page_content for t in documentos_chunks], embeddings, index_name=index_name)
+    '''
 
-    # Conectarse con los datos ya existentes en Pinecone
+    #Coneccion Pinecone
     pinecone.init(
         api_key = '4f58d18b-75ff-4404-94bc-832bf24c45d1',
         environment = 'asia-southeast1-gcp-free'
     )
     index_name = 'tid'
-    embeddings = OpenAIEmbeddings(openai_api_key='OPENAI_APIKEY')
-    docsearch1 = Pinecone.from_existing_index(index_name, embeddings)
-
-    # OpenAI LLM + LangChain para el QA
+    #Coneccion OpenAI (LLM y Embeddings)
     llm = OpenAI(
         temperature = 0,
         openai_api_key = 'OPENAI_APIKEY',
         )
     index_name = 'tid1'
+    embeddings = OpenAIEmbeddings(openai_api_key='OPENAI_APIKEY')
 
-    # Para cargar los datos a pinecone
-    docsearch = Pinecone.from_texts([t.page_content for t in documentos_chunks], embeddings, index_name=index_name)
-
-    # Para conectarse con los datos ya existentes en Pinecone
     docsearch1 = Pinecone.from_existing_index(index_name, embeddings)
-
-    # OpenAI LLM + LangChain para el QA
-    llm = OpenAI(
-        temperature = 0,
-         openai_api_key = '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',
-            )
+    
     chain = load_qa_chain(llm, chain_type="stuff")
     query = f"Describa en formato de regla, 5 relaciones posible entre {word_1} y {word_2}"
     print(query)
